664f12206ab6a8a79b021b74c48c8f5d388f81ee
Anwesh43 <anweshthecool0@gmail.com>
2018-12-26 18:45:57 +0530

    adding scale functions

--- Plus180ExpanderStage.ts
+++ Plus180ExpanderStage.ts
@@ -5,6 +5,21 @@ const strokeFactor : number = 90
 const sizeFactor : number = 3
 const color : string = "#4527A0"
 
+const maxScale : Function = (scale : number, i : number, n : number) : number => Math.max(0, scale - i / n)
+
+const divideScale : Function = (scale : number, i : number, n : number) : number => Math.min(1/n, maxScale(scale, i, n))
+
+const scaleFactor : Function = (scale : number) : number => Math.floor(this / scDiv)
+
+const mirrorValue : Function = (scale : number, a : number, b : number) : number => {
+    const k : number = scaleFactor(scale)
+    return (1 - k)/a + k / b
+}
+
+const updateScale : Function = (scale : number, dir : number, a : number, b : number) : number => {
+    return mirrorValue(scale, a, b) * scGap * dir
+}
+
 class Plus180ExpanderStage {
     canvas : HTMLCanvasElement = document.createElement('canvas')
     context : CanvasRenderingContext2D
