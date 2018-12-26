5ac021f0dd25f7d3c97437dacab13001302c79a6
Anwesh43 <anweshthecool0@gmail.com>
2018-12-26 19:05:20 +0530

    ading State class

--- Plus180ExpanderStage.ts
+++ Plus180ExpanderStage.ts
@@ -81,3 +81,24 @@ class Plus180ExpanderStage {
         stage.handleTap()
     }
 }
+
+class State {
+    scale : number = 0
+    dir : number = 0
+    prevScale : number = 0
+    update(cb : Function) {
+        this.scale += updateScale(this.scale, lines, lines)
+        if (Math.abs(this.scale - this.prevScale) > 1) {
+            this.scale = this.prevScale + this.dir
+            this.dir = 0
+            this.prevScale = this.scale
+        }
+    }
+
+    startUpdating(cb : Function) {
+        if (this.dir ==  0) {
+            this.dir = 1 - 2 * this.prevScale
+            cb()
+        }
+    }
+}
